008b446b575e6767c7b162ce7b941aa7b5f387f5
János Gönye <janos.gonye@gmail.com>
2020-04-18 18:51:24 +0200

    generate 'path_out' if None

--- rg_hist/__init__.py
+++ rg_hist/__init__.py
@@ -1,24 +1,30 @@
+import os
+
 import cv2
 
 from rg_hist.core import calc_img_rg_hist, rg_color_space
 from rg_hist.gamma import gamma_decode
 from rg_hist.utils import copy_luminosity
 
-def draw_rg_hist(path_in, path_out=None, a1=0, a2=.9975, size=256, blur=9):
+
+def draw_rg_hist(path_in, path_out=None, a1=0, a2=.9975, size=256, blur=0):
     img = cv2.imread(path_in)
     if img is None:
-        raise ValueError(f"Image not found '{path_in}'")
+        raise ValueError("Image not found '{path_in}'".format(path=path_in))
     if not (0 <= a1 <= 1) or not (0 <= a2 <= 1):
         raise ValueError("'a1' and 'a2' must between the values '0' and '1'")
     if a1 >= a2:
         raise ValueError("'a1' can't be larger than 'a2' or equal to it")
 
     if path_out is None:
-        path_out = 'hist_' + path_in
+        head, tail = os.path.split(path_in)
+        name, ext = tail.split(".")
+        path_out = os.path.join(head, name + '_hist' + '.' + ext)
 
     img_rg = calc_img_rg_hist(img, size)
     rg_space = rg_color_space(size)
     img_rg = copy_luminosity(img_rg, rg_space)
-    img_rg = cv2.blur(img_rg, (blur, blur))
+    if blur > 0:
+        img_rg = cv2.blur(img_rg, (blur, blur))
     img_rg = gamma_decode(img_rg)
     cv2.imwrite(path_out, img_rg)
