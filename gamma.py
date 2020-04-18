66ac8d1156e99a085dec01e6819927a6e6690bea
János Gönye <janos.gonye@gmail.com>
2020-04-18 17:21:48 +0200

    add a module to handle gamma correction related stuff

---
+++ gamma.py
@@ -0,0 +1,14 @@
+GAMMA_DECODE_EXPONENT = 2.2
+GAMMA_ENCODE_EXPONENT = 1 / 2.2
+
+
+def gamma_correct(img, exponent):
+    return ((img.copy() / 255.0) ** exponent) * 255
+
+
+def gamme_encode(img):
+    return gamma_correct(img, exponent=GAMMA_ENCODE_EXPONENT)
+
+
+def gamma_decode(img):
+    return gamma_correct(img, exponent=GAMMA_DECODE_EXPONENT)
