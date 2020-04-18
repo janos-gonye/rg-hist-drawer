cec43616155b0e4dca4f93d451e8884851bccdc7
János Gönye <janos.gonye@gmail.com>
2020-04-18 16:48:21 +0200

    implement min-max contrast steching

--- core.py
+++ core.py
@@ -36,3 +36,20 @@ def calc_rg_hist(x, y, bin_):
     """
     return histogram_2d(y.ravel(), x.ravel(),
                         bins=[bin, bin], range=[[0, 1], [0, 1]])
+
+
+def contrast_stretch(array, a1, a2):
+    """Perform min-max constrast stretching on the given 'array' array.
+
+    Typical use:
+        # 1. Get rid of the brightest 5% of the values. 
+        contrast_stretch(luminosity, a1=0, a2=.95)
+        # 2. Get rid of the less brightest 20% of the values.
+        contrast_stretch(luminosity, a1=.2, a2=1)
+    """
+    array_1D = np.sort(np.ravel(array))
+	min_ = array_1D[int(a1 * array_1D.shape[0])]
+	max_ = array_1D[int(a2 * array_1D.shape[0])]
+	array[array < min_] = min_  # Handle underflow
+	array[array > max_] = max_  # Handle overflow
+	return array
